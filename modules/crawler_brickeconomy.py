2a27439c1d55babe279d447f5204f40a06f05a67
bolee <dlqhdgkr456@gmail.com>
2022-07-21 21:32:11 +0900

    minifigs.blog crawler update

--- modules/crawler_brickeconomy.py
+++ modules/crawler_brickeconomy.py
@@ -19,7 +19,7 @@ chrome_options.add_argument('--disable-gpu')
 #chrome_options.add_argument('--window-size=1920,1080')
 
 # 2. OS 에 맞춰 드라이버 세팅 (버전 체크 필요)
-if platform.system() == 'Windows': 
+if platform.system() == 'Windows':
 	# driver_link = os.getcwd() + '/factory/modules/driver/chromedriver.exe'
 	driver_link = "C:/chromedriver.exe"
 else:
@@ -63,8 +63,7 @@ def find_lego(idx):
 	time.sleep(1)
 	return theme, minifigs_serial
 
-if __name__ == "__main__":
-
+def main():
 	base_url = 'https://www.brickeconomy.com/minifigs'
 	driver.get(base_url)
 	for i in range(1, 126):
@@ -72,17 +71,21 @@ if __name__ == "__main__":
 		theme, minifigs_serial = find_lego(i) #1~125, 중분류 개수
 		minifigs.extend(minifigs_serial)
 
-	if platform.system() == 'Windows':
-		if not os.path.exists(f"C:/SGM_AI/42Brick/factory/img/{theme}"):
-			os.makedirs(f"C:/SGM_AI/42Brick/factory/img/{theme}")
-	else:
-		if not os.path.exists(os.getcwd() + f"/factory/img/{theme}"):
-			os.makedirs(os.getcwd() + f"/factory/img/{theme}")
+		if platform.system() == 'Windows':
+			if not os.path.exists(f"C:/SGM_AI/42Brick/factory/img/{theme}"):
+				os.makedirs(f"C:/SGM_AI/42Brick/factory/img/{theme}")
+		else:
+			if not os.path.exists(os.getcwd() + f"/factory/img/{theme}"):
+				os.makedirs(os.getcwd() + f"/factory/img/{theme}")
 
-	opener = urllib.request.build_opener()
-	opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-	urllib.request.install_opener(opener)
+		opener = urllib.request.build_opener()
+		opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+		urllib.request.install_opener(opener)
 
-	for ele in minifigs:
-		img = 'https://www.brickeconomy.com/resources/images/minifigs/' + ele + '_large.jpg'
-		urllib.request.urlretrieve(img, "C:/SGM_AI/42Brick/factory/img/{0}/{1}.jpg".format(theme, ele))
+		for ele in minifigs:
+			img = 'https://www.brickeconomy.com/resources/images/minifigs/' + ele + '_large.jpg'
+			urllib.request.urlretrieve(img, "C:/SGM_AI/42Brick/factory/img/{0}/{1}.jpg".format(theme, ele))
+
+
+if __name__ == "__main__":
+	main()
