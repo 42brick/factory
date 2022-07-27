47d5e736de306203bed099fe37b1878089350e1f
susie-Choi <sschoidev@gmail.com>
2022-07-27 13:21:17 +0900

    setting user

--- modules/preprocessor.py
+++ modules/preprocessor.py
@@ -14,22 +14,23 @@ def plt_test(img_path):
     for face in resp:
         plt.imshow(face)
         plt.show()
-    # x1, y1 = resp["face_1"]["landmarks"]["right_eye"]
-    # x2, y2 = resp["face_1"]["landmarks"]["left_eye"]
 
-    # y = abs(y1 - y2)
-    # x = abs(x2 - x1)
-    # sq = math.sqrt(y**2 + x**2)
+    x1, y1 = resp["face_1"]["landmarks"]["right_eye"]
+    x2, y2 = resp["face_1"]["landmarks"]["left_eye"]
 
-    # cos_alpha = (x**2 + sq**2 - y**2) / (2*x*sq)
-    # alpha = np.arccos(cos_alpha)
-    # alpha = (alpha * 180) / math.pi
+    y = abs(y1 - y2)
+    x = abs(x2 - x1)
+    sq = math.sqrt(y**2 + x**2)
 
-    # aligned_img = Image.fromarray(img)
-    # aligned_img = np.array(aligned_img.rotate(alpha))
+    cos_alpha = (x**2 + sq**2 - y**2) / (2*x*sq)
+    alpha = np.arccos(cos_alpha)
+    alpha = (alpha * 180) / math.pi
 
-    # plt.imshow(aligned_img)
-    # plt.show()
+    aligned_img = Image.fromarray(img)
+    aligned_img = np.array(aligned_img.rotate(alpha))
+
+    plt.imshow(aligned_img)
+    plt.show()
 
 def main():
     img_path = "C:/SGM_AI/42Brick/factory/img_human_filter/4-juniors/4j001.jpg"
